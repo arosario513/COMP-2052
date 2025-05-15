716433b24aeb8117d50e6cb1e6e16dc4b215c37c
arosario513 <a.rosario51304@gmail.com>
2025-05-15 16:41:03 -0400

    Minor tweaks

--- work/final/setup.sh
+++ work/final/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-mkdir mass
+set -e
+mkdir -p mass
 cd mass
 git init
 git branch -m main
 git remote add origin https://github.com/arosario513/COMP-2052.git
-git sparse-checkout init
-git sparse-checkout add work/final
+git sparse-checkout init --cone
+git sparse-checkout set work/final
 git pull origin main
-mv ./work/final ..
+mv ./work/final ../final
 cd ..
 rm -rf mass
-
